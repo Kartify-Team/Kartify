fd999ead5cb4e47efd9eb4a96aa8e6b8b7187302
David <david.lam296@gmail.com>
2019-12-09 16:16:13 -0500

    Create placeholder template react elements

---
+++ client/components/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import OtherItems from "./OtherItems";
+import Overview from "./Overview";
+import Questions from "./Questions";
+import Reviews from "./Reviews";
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <Overview />
+        <OtherItems />
+        <Questions />
+        <Reviews />
+      </div>
+    );
+  }
+}
---
+++ client/components/OtherItems/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default class OtherItems extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return <div>Other Items Component</div>;
+  }
+}
