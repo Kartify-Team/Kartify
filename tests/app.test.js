633bf0af5e73a396b7061b63049dced06d53501d
David <david.lam296@gmail.com>
2019-12-09 19:58:04 -0500

    Test CI

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,9 +1,13 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import App from '../client/components/App.jsx';
+import React from "react";
+import { shallow } from "enzyme";
+import App from "../client/components/App.jsx";
 
-describe('First React component test with Enzyme', () => {
-   it('renders without crashing', () => {
-      shallow(<App />);
-    });
+describe("First React component test with Enzyme", () => {
+  it("renders without crashing", () => {
+    shallow(<App />);
+  });
+
+  it("this is destined to fail", () => {
+    expect(true).toEqual(false);
+  });
 });
