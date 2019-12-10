736f7993f6924728ed4535bccbe16e00c73dddc5
Bryce Champaign <bechampaign@protonmail.com>
2019-12-10 15:36:39 -0500

    Unimplement routes and unnecessary middleware
    Since the client will be interfacing with the API directly, there is no need for the routes contained in the
    routes.js file as well as some middleware modules like body-paser. Tests related to the server have also been removed.

--- server/index.js
+++ server/index.js
@@ -1,13 +1,9 @@
 require('dotenv').config();
 const express = require('express');
-const bodyParser = require('body-parser');
 const path = require('path');
 const server = express();
-const router = require('./routes.js');
 const port = process.env.PORT;
 
 server.use(express.static(path.join(__dirname, '../dist')));
-server.use(bodyParser.json());
-server.use(router);
 
 server.listen(port, () => console.log(`Listening on port ${port}`));
