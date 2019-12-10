f842032321477e384c25b125d96fe8a32497ca4a
Elaine <eyw2008@gmail.com>
2019-12-09 19:59:23 -0500

    dotenv setup

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
+require('dotenv').config()
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const server = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 server.use(express.static(path.join(__dirname, '../dist')));
 server.use(bodyParser.json());
