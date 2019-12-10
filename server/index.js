7b5443515ab400a5fd51e65560656d9dc25cbf60
Bryce Champaign <bechampaign@protonmail.com>
2019-12-10 11:03:07 -0500

    Undo bad fix

--- server/index.js
+++ server/index.js
@@ -1,6 +1,4 @@
-if (!process.env.PORT) {
-  require('dotenv').config();
-}
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
