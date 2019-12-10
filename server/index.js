28dcb351e6cfe3721c6c8ec21548cb3f3d332fc2
Bryce Champaign <bechampaign@protonmail.com>
2019-12-10 10:53:37 -0500

    Fix error when no dotenv file is found

--- server/index.js
+++ server/index.js
@@ -1,4 +1,6 @@
-require('dotenv').config();
+if (!process.env.PORT) {
+  require('dotenv').config();
+}
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
