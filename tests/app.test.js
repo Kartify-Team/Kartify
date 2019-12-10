decd0b1b9e3c8af82244d0d6575b96e2116fdfb5
David <david.lam296@gmail.com>
2019-12-10 10:27:06 -0500

    Fix bug in app test

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { shallow } from "enzyme";
+import { shallow, mount } from "enzyme";
 import App from "../client/components/App.jsx";
 
 describe("App Component", () => {
