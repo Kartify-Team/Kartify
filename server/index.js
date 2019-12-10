b9dca157cd3611ed9240d2fc496bed6479c34290
Bryce Champaign <bechampaign@protonmail.com>
2019-12-10 08:51:37 -0500

    Implement router

--- server/index.js
+++ server/index.js
@@ -2,9 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const server = express();
+const router = require('./routes.js');
 const port = process.env.PORT || 3000;
 
 server.use(express.static(path.join(__dirname, '../dist')));
 server.use(bodyParser.json());
+server.use(router);
 
 server.listen(port, () => console.log(`Listening on port ${port}`));
