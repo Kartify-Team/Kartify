0616f4cc7515318f7624da511e253fc7e452c663
Bryce Champaign <bechampaign@protonmail.com>
2019-12-11 12:11:55 -0500

    Implement getProductRatings function
    getProductRatings accepts a product id and returns an object with the count for each review star rating as well as the average review rating for the product associated with
    the input id.

---
+++ client/greenfieldAPI/reviews.js
@@ -0,0 +1,35 @@
+import axios from 'axios';
+
+const greenfieldRoot = 'http://3.134.102.30';
+
+export const getProductRatings = productId => {
+  const getReviewMetadata = axios
+    .get(`${greenfieldRoot}/reviews/${productId}/meta`)
+    .then(response => response.data.ratings);
+
+  return getReviewMetadata.then(ratings => {
+    const oneCount = ratings[1] || 0;
+    const twoCount = ratings[2] || 0;
+    const threeCount = ratings[3] || 0;
+    const fourCount = ratings[4] || 0;
+    const fiveCount = ratings[5] || 0;
+    const sum = oneCount + twoCount + threeCount + fourCount + fiveCount;
+
+    const average =
+      (oneCount +
+        twoCount * 2 +
+        threeCount * 3 +
+        fourCount * 4 +
+        fiveCount * 5) /
+      sum;
+
+    return {
+      1: oneCount,
+      2: twoCount,
+      3: threeCount,
+      4: fourCount,
+      5: fiveCount,
+      average
+    };
+  });
+};
