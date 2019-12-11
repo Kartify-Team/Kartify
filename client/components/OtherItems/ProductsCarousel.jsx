83eee4d080c4cf9ae162b718061b351926521f79
Sebastian Coronel <scoronelhamilton@gmail.com>
2019-12-11 10:27:49 -0500

    Fix bug in carousel

--- client/components/OtherItems/ProductsCarousel.jsx
+++ client/components/OtherItems/ProductsCarousel.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 // import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel';
 // import { Carousel } from 'react-responsive-carousel';
 // import Carousel, { Dots } from '@brainhubeu/react-carousel';
-import Slider from "react-slick";
+// import Slider from "react-slick";
 
 
 export default class ProductsCarousel extends React.Component {
@@ -12,16 +12,16 @@ export default class ProductsCarousel extends React.Component {
   }
 
   render() {
-    const settings = {
-      dots: false, // ?
-      infinite: false,
-      speed: 500,
-      slidesToShow: 3,
-      slidesToScroll: 3
-    };
+    // const settings = {
+    //   dots: false, // ?
+    //   infinite: false,
+    //   speed: 500,
+    //   slidesToShow: 3,
+    //   slidesToScroll: 3
+    // };
     return (
       <div>
-        <Slider {...settings}>
+        {/* <Slider {...settings}>
           <div>
             <h3>1</h3>
           </div>
@@ -49,7 +49,7 @@ export default class ProductsCarousel extends React.Component {
           <div>
             <h3>9</h3>
           </div>
-        </Slider>
+        </Slider> */}
       </div>
     );
   }
