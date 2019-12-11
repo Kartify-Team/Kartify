e252efbb2edab9be8c9d3d175e00024393bc7db4
Bryce Champaign <bechampaign@protonmail.com>
2019-12-11 14:33:04 -0500

    Implement Stars component
    Stars accepts a prop called "stars" and renders 5 stars with an overlay of stars depending on the value of the stars prop. It can be used to display the star ratings of products.

---
+++ client/components/Reviews/Stars.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Stars = ({ stars }) => {
+  const percentage = (Math.round(((stars * 100) / 5) * 4) / 4).toFixed(2);
+  const topStyle = {
+    width: `${percentage}%`
+  };
+  return (
+    <div className="star-container">
+      <span className="bottom-star">
+        <span className="fa fa-star-o"></span>
+        <span className="fa fa-star-o"></span>
+        <span className="fa fa-star-o"></span>
+        <span className="fa fa-star-o"></span>
+        <span className="fa fa-star-o"></span>
+      </span>
+      <span className="top-star" style={topStyle}>
+        <span className="fa fa-star"></span>
+        <span className="fa fa-star"></span>
+        <span className="fa fa-star"></span>
+        <span className="fa fa-star"></span>
+        <span className="fa fa-star"></span>
+      </span>
+    </div>
+  );
+};
+
+export default Stars;
