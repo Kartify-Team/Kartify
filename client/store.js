cc22374d3f860fc67c5f44595b6b3f50b3e9894e
Elaine <eyw2008@gmail.com>
2019-12-11 18:58:19 -0500

    comment out tests

--- client/store.js
+++ client/store.js
@@ -4,8 +4,9 @@ import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
 let store = createStore(rootReducer, {},
-    // applyMiddleware(thunk));
-    compose(applyMiddleware(thunk),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
+    applyMiddleware(thunk));
+//// Dev tools cause tests to fail
+// compose(applyMiddleware(thunk),
+//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
 
 export default store;
