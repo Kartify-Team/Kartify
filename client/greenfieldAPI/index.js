6adb4af13fb833d974d1f62d59e60cb8e8313280
Bryce Champaign <bechampaign@protonmail.com>
2019-12-11 12:15:09 -0500

    Add getProductRatings stub to index.js and reformat some code

--- client/greenfieldAPI/index.js
+++ client/greenfieldAPI/index.js
@@ -1,16 +1,16 @@
 import axios from 'axios';
 import questions from './questions';
+import { getProductRatings } from './reviews.js';
 
 const greenfieldRoot = 'http://3.134.102.30';
 
 export default {
-  getProductInfo: (id) => {
+  getProductInfo: id => {
     return axios.get(`${greenfieldRoot}/products/${id}`);
   },
-  getRelatedProducts: (id) => {
+  getRelatedProducts: id => {
     return axios.get(`${greenfieldRoot}/products/${id}/related`);
   },
-  getQuestions: (id) => {
-    return questions.getQuestions(id);
-  }
+  getQuestions: id => questions.getQuestions(id),
+  getReviewInfo: id => getProductRatings(id)
 };
