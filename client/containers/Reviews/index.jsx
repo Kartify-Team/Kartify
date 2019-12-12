db139061089b5997e917f0f123797a84e47b8e76
Elaine <eyw2008@gmail.com>
2019-12-12 15:19:10 -0500

    changed redux files to .js

--- client/containers/Reviews/index.jsx
+++
@@ -1,8 +0,0 @@
-import { connect } from 'react-redux';
-import Reviews from '../../components/Reviews';
-import store from '../../store.js';
-
-const mapStateToProps = store => ({ product: store.product });
-
-const ReviewsContainer = connect(mapStateToProps)(Reviews);
-export default ReviewsContainer;
