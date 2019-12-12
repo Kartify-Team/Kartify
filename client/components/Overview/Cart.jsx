1dc312203e4b6c3c9ebd0fed678b9f86ba10249e
David <david.lam296@gmail.com>
2019-12-11 20:01:57 -0500

    Style subcomponents

--- client/components/Overview/Cart.jsx
+++ client/components/Overview/Cart.jsx
@@ -1,5 +1,24 @@
 import React from 'react';
 
-const Cart = props => <div className="interaction-item">Cart</div>;
+const Cart = props => (
+  <div className="interaction-item" id="cart-container">
+    <select id="size-button">
+      <option value="none" selected disabled hidden>
+        SELECT SIZE
+      </option>
+    </select>
+
+    {/* QUANTITY SELECTOR */}
+    <select id="qty-button">
+      <option>1</option>
+    </select>
+    <btn id="add-button" className="overview-button">
+      ADD TO BAG
+    </btn>
+    <btn id="star-button" className="overview-button">
+      O
+    </btn>
+  </div>
+);
 
 export default Cart;
