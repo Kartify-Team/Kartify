6addfa3a46e89d881e8b99459afad3d61f790028
Bryce Champaign <bechampaign@protonmail.com>
2019-12-11 20:49:41 -0500

    reformat store.js code

--- client/store.js
+++ client/store.js
@@ -1,10 +1,8 @@
-
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
-let store = createStore(rootReducer, {},
-    applyMiddleware(thunk));
+let store = createStore(rootReducer, {}, applyMiddleware(thunk));
 // compose(applyMiddleware(thunk),
 // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
 
