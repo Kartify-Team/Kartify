bebf30980584fcb4f964a88f76e3ee5aaecdae26
Bryce Champaign <bechampaign@protonmail.com>
2019-12-11 20:55:12 -0500

    Remove console.log statements

--- client/actions/creators/addQuestionList.js
+++ client/actions/creators/addQuestionList.js
@@ -1,21 +1,13 @@
-import { ADD_QUESTION_LIST } from "..";
-import greenfieldAPI from '../../greenfieldAPI'
+import { ADD_QUESTION_LIST } from '..';
+import { getQuestions } from '../../greenfieldAPI';
 
-export const addQuestionList = (productId) => {
-
-    console.log("line 6")
-    return function thunk(dispatch) {
-        console.log("thunk")
-        return greenfieldAPI
-            .getQuestions(productId)
-            .then((questions) => {
-                console.log("run")
-                return dispatch({
-                    type: 'ADD_QUESTION_LIST',
-                    questions: questions
-                })
-            });
-    }
-}
-
-
+export const addQuestionList = productId => {
+  return function thunk(dispatch) {
+    return getQuestions(productId).then(questions => {
+      return dispatch({
+        type: 'ADD_QUESTION_LIST',
+        questions: questions
+      });
+    });
+  };
+};
