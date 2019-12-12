12a2967b96dbb7c2720bd7175fe932bad8252b4e
Bryce Champaign <bechampaign@protonmail.com>
2019-12-11 22:45:35 -0500

    Change tests to be more applicable, refactored store
    Tests now use mock store.

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,15 +1,44 @@
 import React from 'react';
 import { shallow, mount } from 'enzyme';
 import App from '../client/components/App.jsx';
+import AppContainer from '../client/containers/App';
 import { Provider } from 'react-redux';
-import store from '../client/store';
+import configureStore from 'redux-mock-store';
+import { MemoryRouter } from 'react-router-dom';
+import OverviewContainer from '../client/containers/Overview/index.jsx';
+import thunk from 'redux-thunk';
+import { changeProduct } from '../client/actions/creators/changeProduct';
+
+const mockStore = configureStore([thunk]);
 
 describe('App Component', () => {
-  it('renders', () => {
-    shallow(<Provider store={store}><App /></Provider>);
+  let store;
+  beforeEach(() => {
+    store = mockStore({
+      product: {
+        info: {
+          id: 1,
+          name: 'Camo Onesie',
+          slogan: 'Blend in to your crowd',
+          description:
+            'The So Fatigues will wake you up and fit you in. This high energy camo will have you blending in to even the wildest surroundings.',
+          category: 'Jackets',
+          default_price: '140',
+          features: [
+            {
+              feature: 'Buttons',
+              value: 'Brass'
+            }
+          ]
+        }
+      },
+      otherItems: {},
+      questionsReducer: {}
+    });
   });
 
-  it('renders all child components', () => {
-    mount(<Provider store={store}><App /></Provider>);
+  it('passes productInfo from store to App props', () => {
+    const wrapper = shallow(<AppContainer store={store} />);
+    expect(wrapper.find('App').prop('productInfo').id).toEqual(1);
   });
 });
