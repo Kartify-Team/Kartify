0f73fd6d6701f75370bb4dd27df3ae406228f2d9
Bryce Champaign <bechampaign@protonmail.com>
2019-12-12 21:05:23 -0500

    Implement setCharacteristics in Reviews component
    setCharacteristics will update state with the correct characteristics for the current product id when the product prop is udpated.

--- client/components/Reviews/index.jsx
+++ client/components/Reviews/index.jsx
@@ -1,26 +1,27 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import ReviewList from './ReviewList';
 import RatingBreakdown from './RatingBreakdown';
 
-export default class Reviews extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
+const Reviews = ({ product, characteristics, setCharacteristics }) => {
+  if (product) {
+    useEffect(() => {
+      setCharacteristics(product.id);
+    }, []);
   }
 
-  render() {
-    if (!this.props.product) {
-      return null;
-    } else {
-      return (
-        <>
-          <h3>Ratings & Reviews</h3>
-          <div id="reviews-container">
-            <RatingBreakdown productId={this.props.product.id} />
-            <ReviewList />
-          </div>
-        </>
-      );
-    }
+  if (!product) {
+    return null;
+  } else {
+    return (
+      <>
+        <h3>Ratings & Reviews</h3>
+        <div id="reviews-container">
+          <RatingBreakdown productId={product.id} />
+          <ReviewList />
+        </div>
+      </>
+    );
   }
-}
+};
+
+export default Reviews;
