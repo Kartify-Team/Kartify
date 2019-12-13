1d0dc34f4d49449da6dc9d42f235622567fc28a8
Sebastian Coronel <scoronelhamilton@gmail.com>
2019-12-12 21:41:51 -0500

    Fix infinite rendering in app

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -6,10 +6,13 @@ import ReviewsContainer from '../containers/Reviews';
 import QuestionsContainer from '../containers/Questions';
 
 const App = ({ changeProduct, productInfo, setRatings }) => {
-  changeProduct(useParams().id || 1);
-  setRatings(useParams().id);
 
-  console.log('APP RENDERING!'); // TODO: FIX APP INFINITE RENDERING
+  const id = useParams().id;
+  useEffect(()=> {
+    changeProduct(id);
+    setRatings(id);
+  }, []);
+
   return (
     <div id="components-container">
       <OverviewContainer />
