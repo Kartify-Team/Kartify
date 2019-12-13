7a75e3336097b354ea716fc12bbb153a63a0196c
Bryce Champaign <bechampaign@protonmail.com>
2019-12-12 19:58:12 -0500

    Implement setRatings in App

--- client/containers/App/index.js
+++ client/containers/App/index.js
@@ -1,13 +1,15 @@
 import { connect } from 'react-redux';
 import App from '../../components/App';
 import { changeProduct } from '../../actions/creators/changeProduct';
+import { setRatings } from '../../actions/creators/setRatings';
 
 const mapStateToProps = state => ({
   productInfo: state.product
 });
 
 const mapDispatchToProps = dispatch => ({
-  changeProduct: productId => dispatch(changeProduct(productId))
+  changeProduct: productId => dispatch(changeProduct(productId)),
+  setRatings: productId => dispatch(setRatings(productId))
 });
 
 const AppContainer = connect(mapStateToProps, mapDispatchToProps)(App);
