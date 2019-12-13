e5e4e34e86c46fc5aa34e190628462e80d298433
Bryce Champaign <bechampaign@protonmail.com>
2019-12-13 11:38:36 -0500

    Refactor RatingBreakdown
    RatingBreakdown now accesses ratings differently and sets the total ratings on render

--- client/components/Reviews/RatingBreakdown.jsx
+++ client/components/Reviews/RatingBreakdown.jsx
@@ -2,21 +2,29 @@ import React, { useState, useEffect } from 'react';
 import StarStats from './StarStats';
 import SizeRating from './SizeRating';
 import ComfortRating from './ComfortRating';
-import { getProductRatings } from '../../greenfieldAPI/reviews';
+import { getTotalRatings } from '../../utils';
 
-const RatingBreakdown = ({ productId }) => {
-  const [rating, setRating] = useState(0);
+const RatingBreakdown = ({ productId, ratings }) => {
+  let [totalRatings, setTotalRatings] = useState(0);
   useEffect(() => {
-    getProductRatings(productId).then(ratingData => setRating(ratingData));
+    if (ratings.ratings !== undefined && ratings.ratings !== null) {
+      setTotalRatings(getTotalRatings(ratings.ratings));
+    }
   }, []);
 
-  return (
-    <div id="review-stats-container">
-      <StarStats rating={rating.average} />
-      <SizeRating />
-      <ComfortRating />
-    </div>
-  );
+  if (ratings === null || ratings === undefined) {
+    return null;
+  } else {
+    return (
+      <div id="review-stats-container">
+        <StarStats
+          rating={ratings.ratings === null ? null : ratings.ratings.average}
+        />
+        <SizeRating />
+        <ComfortRating />
+      </div>
+    );
+  }
 };
 
 export default RatingBreakdown;
