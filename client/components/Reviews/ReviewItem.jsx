e6b35df3015548618e6a62174b9f87c0a949628e
Bryce Champaign <bechampaign@protonmail.com>
2019-12-13 14:04:59 -0500

    Create ReviewItem component for displaying individual reviews

--- client/components/Reviews/ReviewItem.jsx
+++ client/components/Reviews/ReviewItem.jsx
@@ -1,1 +1,25 @@
 import React from 'react';
+import Stars from './Stars';
+
+const ReviewItem = ({
+  body,
+  date,
+  helpfulness,
+  rating,
+  summary,
+  reviewer_name,
+  review_id
+}) => {
+  return (
+    <div key={review_id} className="review_item">
+      <p>{reviewer_name}</p>
+      <p>{summary}</p>
+      <p>{body}</p>
+      <p>Helpfulness: {helpfulness}</p>
+      <p>{date.toLocaleString('en-US')}</p>
+      <Stars stars={rating} />
+    </div>
+  );
+};
+
+export default ReviewItem;
