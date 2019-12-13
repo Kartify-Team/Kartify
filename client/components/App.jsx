c8807ff2847833356c0a811973b1b32310f34343
Bryce Champaign <bechampaign@protonmail.com>
2019-12-12 21:37:34 -0500

    Fix app re-rendering constantly by placing state-updating functions in a hook

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, useParams } from 'react-router-dom';
 import OverviewContainer from '../containers/Overview';
 import OtherItemsContainer from '../containers/OtherItems';
 import ReviewsContainer from '../containers/Reviews';
 import QuestionsContainer from '../containers/Questions';
 
-const App = ({ changeProduct, productInfo, setRatings }) => {
-  changeProduct(useParams().id || 1);
-  setRatings(useParams().id);
+const App = ({
+  changeProduct,
+  productInfo,
+  setRatings,
+  setCharacteristics
+}) => {
+  const id = useParams().id;
+  useEffect(() => {
+    changeProduct(id);
+    setRatings(id);
+  }, []);
+
   return (
     <div id="components-container">
       <OverviewContainer />
