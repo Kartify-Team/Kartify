6189fd12bad39f47ccd67c09461d78fd154c7409
Bryce Champaign <bechampaign@protonmail.com>
2019-12-13 12:24:16 -0500

    Pass setReviewList dispatcher to App component props
    App will update the reviewList state property on render using setReviewList

--- client/containers/App/index.js
+++ client/containers/App/index.js
@@ -2,6 +2,7 @@ import { connect } from 'react-redux';
 import App from '../../components/App';
 import { changeProduct } from '../../actions/creators/changeProduct';
 import { setRatings } from '../../actions/creators/setRatings';
+import { setReviewList } from '../../actions/creators/setReviewList';
 
 const mapStateToProps = state => ({
   productInfo: state.product
@@ -9,7 +10,8 @@ const mapStateToProps = state => ({
 
 const mapDispatchToProps = dispatch => ({
   changeProduct: productId => dispatch(changeProduct(productId)),
-  setRatings: productId => dispatch(setRatings(productId))
+  setRatings: productId => dispatch(setRatings(productId)),
+  setReviewList: productId => dispatch(setReviewList(productId, 1, 2))
 });
 
 const AppContainer = connect(mapStateToProps, mapDispatchToProps)(App);
