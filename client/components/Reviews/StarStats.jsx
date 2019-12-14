36ce39b1e7d9db13753145407105a2312356bdc8
Bryce Champaign <bechampaign@protonmail.com>
2019-12-14 17:06:17 -0500

    Fix typo and map ratings to StarBar components with necessary props

--- client/components/Reviews/StarStats.jsx
+++ client/components/Reviews/StarStats.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
-import AverageRating from './AverateRating.jsx';
+import AverageRating from './AverageRating.jsx';
+import StarBar from './StarBar';
 
-const StarStats = ({ rating }) => (
-  <div id="star-stats-container">
-    Star Stats
-    <AverageRating averateRating={rating} />
-  </div>
-);
-
+const StarStats = ({ rating, total, ratings }) => {
+  return (
+    <div id="star-stats-container">
+      Star Stats
+      <AverageRating averageRating={rating} />
+      <div className="stars-breakdown">
+        {Object.keys(ratings).map(rating => {
+          return (
+            <StarBar count={ratings[rating]} total={total} label={rating} />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 export default StarStats;
