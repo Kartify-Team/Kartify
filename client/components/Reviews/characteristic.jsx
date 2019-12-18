69703a8fff6780c3da58ded4a15273f6c6fd5964
Bryce Champaign <bechampaign@protonmail.com>
2019-12-17 22:02:53 -0500

    Implement correct labels for characteristic types

--- client/components/Reviews/characteristic.jsx
+++ client/components/Reviews/characteristic.jsx
@@ -1,14 +1,7 @@
 import React from 'react';
+import { labels } from './labels';
 
 const Characteristic = ({ type, value }) => {
-  let labels = [];
-  if (type === 'Comfort') labels = ['Poor', '', 'Perfect'];
-  if (type === 'Size') labels = ['Too small', 'Perfect', 'Too Large'];
-  if (type === 'Length') labels = ['Too short', 'Perfect', 'Too long'];
-  if (type === 'Width') labels = ['Too narrow', 'Perfect', 'Too wide'];
-  if (type === 'Fit') labels = ['Poor', '', 'Perfect'];
-  if (type === 'Quality') labels = ['Poor', '', 'Great'];
-  // fas fa-caret-down
   const style = {
     width: `${(value / 5) * 100}%`
   };
@@ -20,17 +13,23 @@ const Characteristic = ({ type, value }) => {
         <div className="pointer" style={style}>
           <i
             className="fa fa-caret-down fa-lg"
-            style={{ fontSize: '1.9em' }}
+            style={{ fontSize: '1.2em' }}
           ></i>
         </div>
         <div className="characteristics-sub-bar">
-          <h3 className="characteristic-label">{labels[0]}</h3>
+          <h3 className="characteristic-label">
+            {labels[type.toLowerCase()][0]}
+          </h3>
         </div>
         <div className="characteristics-sub-bar">
-          <h3 className="characteristic-label">{labels[1]}</h3>
+          <h3 className="characteristic-label">
+            {labels[type.toLowerCase()][3]}
+          </h3>
         </div>
         <div className="characteristics-sub-bar">
-          <h3 className="characteristic-label">{labels[2]}</h3>
+          <h3 className="characteristic-label">
+            {labels[type.toLowerCase()][4]}
+          </h3>
         </div>
       </div>
     </div>
