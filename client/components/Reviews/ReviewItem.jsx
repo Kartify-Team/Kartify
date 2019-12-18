e689df089746a9e2b1bde08f56cbee08615e504f
Bryce Champaign <bechampaign@protonmail.com>
2019-12-18 18:11:19 -0500

    Implement button for incrementing the helpful count on a review
    When "yes" is clicked, the review is marked helpful and the list of reviews is rendered again to show this change. The user is then unable to mark the review as helpful again until they refresh the page.
    the "report" button has also been started but is currently not functional.

--- client/components/Reviews/ReviewItem.jsx
+++ client/components/Reviews/ReviewItem.jsx
@@ -1,25 +1,70 @@
 import React from 'react';
 import Stars from './Stars';
 import { formatDate } from '../../utils';
+import {
+  incrementHelpfulness,
+  reportReview
+} from '../../greenfieldAPI/reviews';
 
 const ReviewItem = ({
-  body,
-  date,
-  helpfulness,
-  rating,
-  summary,
-  reviewer_name,
-  review_id
+  review,
+  updateReviews,
+  setReviewsChanged,
+  reviewsChanged,
+  setHelpfulReviews,
+  helpfulReviews
 }) => {
+  const {
+    body,
+    date,
+    helpfulness,
+    reportReview,
+    rating,
+    summary,
+    reviewer_name,
+    review_id
+  } = review;
+
   const displayDate = formatDate(date);
+
+  const handleHelpful = reviewId => {
+    if (helpfulReviews.includes(reviewId)) return;
+
+    const newHelpfulReviews = [...helpfulReviews];
+    newHelpfulReviews.push(reviewId);
+    setHelpfulReviews(newHelpfulReviews);
+
+    incrementHelpfulness(reviewId)
+      .then(() => updateReviews())
+      .then(() => setReviewsChanged(!reviewsChanged));
+  };
+
+  const handleReport = reviewId => {};
+
   return (
-    <div key={review_id} className="review_item">
+    <div className="review_item">
+      <Stars stars={rating} />
       <p>{reviewer_name}</p>
       <p className="summary">{summary}</p>
       <p>{body}</p>
-      <p>Helpfulness: {helpfulness}</p>
       <p>{displayDate.toLocaleString('en-US')}</p>
-      <Stars stars={rating} />
+      Helpful?{' '}
+      <p onClick={() => handleHelpful(review_id)}>
+        <span
+          className={
+            helpfulReviews.includes(review_id) ? '' : 'underline-clickable'
+          }
+        >
+          Yes
+        </span>{' '}
+        ({helpfulness})
+      </p>
+      <p
+        className="underline-clickable"
+        onClick={() => reportReview(review_id)}
+      >
+        Report
+      </p>
     </div>
   );
 };
