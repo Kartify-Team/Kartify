d83fbd903ef744a7f9461fcea1a3ae6331a263cf
Elaine <eyw2008@gmail.com>
2019-12-18 11:57:07 -0500

    localstorage had a bad commit

--- client/utils/localStorage.js
+++ client/utils/localStorage.js
@@ -24,12 +24,12 @@ export const saveOutfit = (id) => {
       localStorage.setItem('outfit', JSON.stringify(newOutfit));
     }
   };
-  export const removeOutfitProduct = (id) => {
-    let outfit = JSON.parse(localStorage.getItem('outfit') || '[]');
-    for (let i = 0; i < outfit.length; i++) {
-      if (outfit[i] === id) {
-        outfit.splice(i, 1);
-      }
-    }
-    localStorage.setItem('outfit', JSON.stringify(outfit));
-  };
+  // export const removeOutfitProduct = (id) => {
+  //   let outfit = JSON.parse(localStorage.getItem('outfit') || '[]');
+  //   for (let i = 0; i < outfit.length; i++) {
+  //     if (outfit[i] === id) {
+  //       outfit.splice(i, 1);
+  //     }
+  //   }
+  //   localStorage.setItem('outfit', JSON.stringify(outfit));
+  // };
