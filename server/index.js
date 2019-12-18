b6f24605356c03fc4cd3e226ea2442b7a1f2666f
Bryce Champaign <bechampaign@protonmail.com>
2019-12-18 18:15:03 -0500

    Implement compression middleware for performance optimization

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const path = require('path');
-const axios = require('axios');
+const compression = require('compression');
 const server = express();
 const root = process.env.ROOT;
 const port = process.env.PORT;
@@ -12,7 +12,7 @@ cloudinary.config({
   api_key: process.env.CLOUD_API_KEY,
   api_secret: process.env.CLOUD_API_SECRET
 });
-
+server.use(compression());
 server.use(express.static(path.join(__dirname, '../dist')));
 
 server.use(require('express-form-data').parse());
@@ -22,11 +22,15 @@ server.get('*', (req, res) => {
 });
 
 server.post('/img', (req, res) => {
-  Promise.all(Object.values(req.files)
-    .map(image => cloudinary.uploader.upload(image.path)))
+  Promise.all(
+    Object.values(req.files).map(image =>
+      cloudinary.uploader.upload(image.path)
+    )
+  )
     .then(cloudResponse => res.send(cloudResponse.map(image => image.url)))
-    .catch((err) => res.status(500).send(err));
+    .catch(err => res.status(500).send(err));
 });
 
-
-server.listen(port, () => console.log(`Server started! ${root}:${port}/products/1`));
+server.listen(port, () =>
+  console.log(`Server started! ${root}:${port}/products/1`)
+);
