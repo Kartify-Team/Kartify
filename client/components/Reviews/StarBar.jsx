785dc04558b051bbf2187bb5efd4b0f654a480f6
Bryce Champaign <bechampaign@protonmail.com>
2019-12-18 15:31:52 -0500

    Implement toggling of review list filters when a rating breakdown label is clicked

--- client/components/Reviews/StarBar.jsx
+++ client/components/Reviews/StarBar.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 
-const StarBar = ({ count, total, label }) => {
+const StarBar = ({ count, total, label, toggleFilter }) => {
   const dynamicStyle = {
     width: `${(count / total) * 100}%`
   };
+
+  const handleClick = filter => {
+    toggleFilter(filter);
+  };
+
   return (
     <>
       <div className="star-bar-container">
-        <span className="star-bar-label">{label} stars</span>
+        <span className="star-bar-label" onClick={() => handleClick(label)}>
+          {label} stars
+        </span>
         <div className="star-bar-static">
           <div className="star-bar-dynamic" style={dynamicStyle}></div>
         </div>
