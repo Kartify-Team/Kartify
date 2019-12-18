23b2b5775fa32450eafc3cecf6fc39bba26ce382
Elaine <eyw2008@gmail.com>
2019-12-17 20:20:08 -0500

    travis yml

--- tests/endToEnd.test.js
+++ tests/endToEnd.test.js
@@ -7,12 +7,17 @@ describe('Kartify Page', () => {
     let page;
 
     beforeAll(async () => {
-        browser = await puppeteer.launch({headless: false});
+        browser = await puppeteer.launch(/*{headless: false}*/);
         page = await browser.newPage();
         await page.goto('http://localhost:3000/products/1?')
     }, timeout)
 
     it('should load without error', async () => {
+        const text = await page.evaluate(() => document.body.textContent);
+        expect(text).toContain('Kartify');
+    })
+
+    it('should be able to ', async () => {
         const text = await page.evaluate(() => document.body.textContent);
         expect(text).toContain('Kartify');
     })
