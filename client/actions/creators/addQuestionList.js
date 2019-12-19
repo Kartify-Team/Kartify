65acae5c94bddd74699bbea72d3c047c5b33ad76
Elaine <eyw2008@gmail.com>
2019-12-19 11:14:24 -0500

    answers by seller on top

--- client/actions/creators/addQuestionList.js
+++ client/actions/creators/addQuestionList.js
@@ -5,7 +5,15 @@ export const addQuestionList = productId => dispatch => {
   return getQuestions(productId).then(questions => {
     questions.forEach((question) =>
       question.answers = Object.values(question.answers)
-        .sort((a, b) => b.helpfulness - a.helpfulness)
+        .sort((a, b) => {
+          if (b.answerer_name === "Seller" && a.answerer_name !== "Seller") {
+            return 1
+          } else if (a.answerer_name === "Seller" && b.answerer_name !== "Seller") {
+            return -1
+          } else {
+            return b.helpfulness - a.helpfulness
+          }
+        })
     );
     return dispatch({
       type: ADD_QUESTION_LIST,
