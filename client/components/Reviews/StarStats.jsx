af422aa2110d3ea6e0793c00e9f765c33cc0d5ca
Bryce Champaign <bechampaign@protonmail.com>
2019-12-18 21:31:31 -0500

    Fix ratings with no count not being displayed
    If a review has no 2 star ratings, the 2-star bar was not being displayed. Now that bar is displayed but is not filled in.

--- client/components/Reviews/StarStats.jsx
+++ client/components/Reviews/StarStats.jsx
@@ -3,16 +3,25 @@ import AverageRating from './AverageRating.jsx';
 import StarBar from './StarBar';
 
 const StarStats = ({ rating, total, ratings, toggleFilter }) => {
+  let stars = {};
+  for (let i = 1; i <= 5; i++) {
+    if (ratings[i] === undefined) {
+      stars[i] = 0;
+    } else {
+      stars[i] = ratings[i];
+    }
+  }
+
   return (
     <div id="star-stats-container">
       <AverageRating averageRating={rating} />
-      {Object.keys(ratings)
+      {Object.keys(stars)
         .reverse()
         .map(rating => {
           return (
             <StarBar
               key={rating}
-              count={ratings[rating]}
+              count={stars[rating]}
               total={total}
               label={rating}
               toggleFilter={toggleFilter}
