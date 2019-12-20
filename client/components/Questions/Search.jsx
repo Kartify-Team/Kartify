31fd0425261736917002a16d2e84e7a9a9fa6041
Elaine <eyw2008@gmail.com>
2019-12-20 14:50:43 -0500

    merge changes

--- client/components/Questions/Search.jsx
+++ client/components/Questions/Search.jsx
@@ -8,18 +8,6 @@ const Search = ({ handleSearch }) => {
       setQuery(text);
       handleSearch(text);
     }
-<<<<<<< HEAD
-  }, [text])
-  return (<>
-    <div id="q-and-a-search-container">
-      <input
-        id="q-and-a-search-input"
-        type="text"
-        placeholder="Have a question? Search for answers…"
-        value={text} onChange={(e) => setText(e.target.value)}
-      />
-      < i className="fa fa-search fa-2x"></i></div></>
-=======
   }, [text]);
   return (
     <>
@@ -34,7 +22,6 @@ const Search = ({ handleSearch }) => {
         <i className="fa fa-search fa-2x"></i>
       </div>
     </>
->>>>>>> 086ebee6a4cba1e433dfa35b7c791c10978ccc12
   );
 };
 
