84eb6d5a25cbb780b656490bedb7eb894699ed2c
David <david.lam296@gmail.com>
2019-12-20 12:21:33 -0500

    Refactor social media buttons

--- client/components/Overview/ProductInteraction.jsx
+++ client/components/Overview/ProductInteraction.jsx
@@ -33,15 +33,19 @@ const ProductInteraction = ({
         )}
         <div id="social-media-buttons">
           <div id="twitter-share">
-            <a
-              href="https://twitter.com/share?ref_src=twsrc%5Etfw"
-              className="twitter-share-button"
-              data-text="Check out our awesome product!"
-              data-hashtags="#Kartify"
-              data-show-count="false"
-            >
-              Tweet
-            </a>
+            <iframe
+              id="twitter-widget-0"
+              scrolling="no"
+              frameBorder="0"
+              className="twitter-share-button twitter-share-button-rendered twitter-tweet-button"
+              style={{
+                visibility: true,
+                width: 61 + "px",
+                height: 20 + "px"
+              }}
+              title="Twitter Tweet Button"
+              src="https://platform.twitter.com/widgets/tweet_button.69e02060c7c44baddf1b5629549acc0c.en.html#dnt=false&amp;hashtags=%23Kartify&amp;id=twitter-widget-0&amp;lang=en&amp;original_referer=http%3A%2F%2Flocalhost%3A3000%2Fproducts%2F5&amp;size=m&amp;text=Check%20out%20our%20awesome%20product!&amp;time=1576858368245&amp;type=share&amp;url=http%3A%2F%2Flocalhost%3A3000%2Fproducts%2F5"
+            ></iframe>
           </div>
           <div
             className="fb-share-button"
@@ -49,13 +53,24 @@ const ProductInteraction = ({
             data-layout="button"
             data-size="small"
           >
-            <a
-              target="_blank"
-              href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;src=sdkpreparse"
-              className="fb-xfbml-parse-ignore"
+            <span
+              style={{
+                verticalAlign: "bottom",
+                width: 67 + "px",
+                height: 20 + "px"
+              }}
             >
-              Share
-            </a>
+              <iframe
+                name="f3225e3ff1c4814"
+                width="1000px"
+                height="1000px"
+                title="fb:share_button Facebook Social Plugin"
+                frameBorder="0"
+                allow="encrypted-media"
+                src="https://www.facebook.com/v5.0/plugins/share_button.php?app_id=&amp;channel=https%3A%2F%2Fstaticxx.facebook.com%2Fconnect%2Fxd_arbiter.php%3Fversion%3D44%23cb%3Df15498d5666a8a%26domain%3Dlocalhost%26origin%3Dhttp%253A%252F%252Flocalhost%253A3000%252Ff2248b93b40d39%26relation%3Dparent.parent&amp;container_width=43&amp;href=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;layout=button&amp;locale=en_US&amp;sdk=joey&amp;size=small"
+                style={{ width: 67 + "px", height: 20 + "px" }}
+              ></iframe>
+            </span>
           </div>
           <div id="pinterest-share">
             <a
