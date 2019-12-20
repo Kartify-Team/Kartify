1d5875906aceab8a4958d70a298324a0a0b6c5af
Bryce Champaign <bechampaign@protonmail.com>
2019-12-19 21:59:31 -0500

    Implement setting totalReviews in store

--- client/containers/Reviews/index.js
+++ client/containers/Reviews/index.js
@@ -4,6 +4,7 @@ import { setCharacteristics } from '../../actions/creators/setCharacteristics';
 import { setReviewList } from '../../actions/creators/setReviewList';
 import { setTotalRatings } from '../../actions/creators/setTotalRatings';
 import { toggleFilter } from '../../actions/creators/toggleFilter';
+import { setTotalReviews } from '../../actions/creators/setTotalReviews';
 
 const mapStateToProps = store => ({
   product: store.product,
@@ -12,7 +13,11 @@ const mapStateToProps = store => ({
   reviews: store.reviews,
   reviewList: store.reviews !== undefined ? store.reviews.reviewList : null,
   totalRatings: store.reviews === undefined ? null : store.reviews.totalRatings,
-  filters: store.reviews && store.reviews.filters ? store.reviews.filters : []
+  filters: store.reviews && store.reviews.filters ? store.reviews.filters : [],
+  totalReviews:
+    store.reviews && store.reviews.totalReviews
+      ? store.reviews.totalReviews
+      : null
 });
 
 const mapDispatchToProps = dispatch => ({
@@ -20,7 +25,8 @@ const mapDispatchToProps = dispatch => ({
   setTotalRatings: ratings => dispatch(setTotalRatings(ratings)),
   setReviewList: (productId, page, count) =>
     dispatch(setReviewList(productId, page, count)),
-  toggleFilter: filter => dispatch(toggleFilter(filter))
+  toggleFilter: filter => dispatch(toggleFilter(filter)),
+  setTotalReviews: totalReviews => dispatch(setTotalReviews(totalReviews))
 });
 
 const ReviewsContainer = connect(mapStateToProps, mapDispatchToProps)(Reviews);
