50cb83c810e9a4cf40a39ff10924aa0b4baf5fea
Sebastian Coronel <scoronelhamilton@gmail.com>
2019-12-20 10:39:36 -0500

    Add buttons to navigate the carousel

--- client/components/OtherItems/ProductsCarousel.jsx
+++ client/components/OtherItems/ProductsCarousel.jsx
@@ -1,20 +1,44 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ProductCard from './ProductCard.jsx';
 
 const ProductsCarousel = ({ products, handleClick, setActionButton }) => {
 
+  const [pointer, setPointer] = useState(0);
+
+  let cardsToRender = 5;
+
   return (
-    <div className="sliderContainer">
-      {products.map((product) => {
-        return (
-          <ProductCard 
-            key={product === null ? 0 : product.id} 
-            product={product} 
-            handleClick={handleClick}
-            setActionButton={setActionButton}
-          />
-        );
+    <div className='sliderContainer'>
+      <i
+        className='fa fa-chevron-left fa-2x leftArrowCarousel'
+        onClick={() => {                
+          setPointer(pointer - 1);
+        }}
+        style={{
+          visibility: pointer > 0 ? 'visible' : 'hidden'
+        }}
+      />
+      {products.map((product, i) => {
+        if (products.length < cardsToRender || (i < pointer + cardsToRender && i >= pointer)) {
+          return (
+            <ProductCard 
+              key={product === null ? 0 : product.id} 
+              product={product} 
+              handleClick={handleClick}
+              setActionButton={setActionButton}
+            />
+          );
+        }
       })}
+      <i
+        className='fa fa-chevron-right fa-2x rightArrowCarousel'
+        onClick={() => {
+          setPointer(pointer + 1);
+        }}
+        style={{
+          visibility: pointer + cardsToRender < products.length ? 'visible' : 'hidden'
+        }}
+      />
     </div>
   );
 };
